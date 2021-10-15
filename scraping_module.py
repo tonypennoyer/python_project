5f48dae6b9620c11f33c78b220c959450b1580b1
Tony Pennoyer <tonypennoyer@gmail.com>
2021-10-15 12:19:26 -0400

    update

--- scraping_module.py
+++ scraping_module.py
@@ -33,20 +33,19 @@ def scrape_rm(soup) :
 	sold_soup = []
 	sold = []
 	date = []
-	country_lst=['USD','EUR','GPB']
+	country_lst=['USD','EUR','GPB', 'CHF']
 
 	headings = soup.find_all('div', {'class': 'search-result__caption'})
 
-	""" This section is to assign the correct date to our date list.
-	this is based on the auctions date and name """
+	""" Assign correct date  """
 	auction = soup.title.text
 	auction = auction.split(' ')
 	if auction[0] == 'Hilton' :
 		date.append('11/05/' + str(auction[2]))
 	elif auction[0] == 'Hershey' :
 		date.append('10/06/' + str(auction[1]))
-	elif auction[0] == 'Arizona' :
-		date.append('01/29/' + str(auction[1]))
+	elif auction[1] == 'Moritz' :
+		date.append('09/17/' + str(auction[2]))
 	elif auction[0] == 'London' :
 		date.append('09/07/' + str(auction[1]))
 	elif auction[1] == 'Fall' :
@@ -63,11 +62,13 @@ def scrape_rm(soup) :
 		date.append('05/05/' + str(auction[2]))
 	elif auction[0] == 'Fort' :
 		date.append('04/03/' + str(auction[2]))
+	elif auction[0] == 'Arizona' :
+		date.append('01/29/' + str(auction[1]))
 
 
+	""" Find all text from soup """
 	for item in headings:
 		Price_Lots.append(item.find('span', {'class':'heading-subtitle--bold ng-binding'}).text)
-
 
 	for item in headings:
 		Model_and_Make.append(item.find('p', {'class':'heading-subtitle--bold ellipsis ng-binding'}).text)
@@ -75,16 +76,9 @@ def scrape_rm(soup) :
 	for item in headings:
 		sold_soup.append(item.find('p', {'class':'clearfix'}).text)
 
-
+	""" Assign correct sold marker """
 	sold_lst = [item.split() for item in sold_soup]
-
 	sold_lst = list(chain.from_iterable(sold_lst))
-
-	# sold_lst = [item.replace('High','') for item in sold_lst]
-	# sold_lst = [item.replace('bidder','') for item in sold_lst]
-	# sold_lst = [item.replace('Bid:','') for item in sold_lst]
-
-	# print(sold_lst)
 
 	for index, elem in enumerate(sold_lst) :
 		if (index+1 < len(sold_lst) and index - 1 >= 0) :
@@ -96,12 +90,13 @@ def scrape_rm(soup) :
 			# print(curr_el)
 			if prev_el in country_lst and curr_el == 'Sold' :
 				sold.append('Sold')
+			elif prev_el[0] == 'C' and curr_el == 'Sold' :
+				sold.append('Sold')
 			if prev_el in country_lst and curr_el == 'Not' and next_el == 'Sold':
 				sold.append('Not Sold')
+			elif prev_el[0] == 'C' and curr_el == 'Not' and next_el == 'Sold':
+				sold.append('Not Sold')
 			else : continue
-
-
-	# print(len(sold))
 
 	""" Cleaning data section """
 	Model_and_Make = [item.split(' ', 1) for item in Model_and_Make]
@@ -120,40 +115,66 @@ def scrape_rm(soup) :
 	Lot, Price = zip(*Price_Lots)
 	Year, Car = zip(*Model_and_Make)
 
-	
+	""" Identifies currency, cleans string and converts to integer """
+	indicator = ''
+
 	for string in Price :
 		if '$' in string:
 			Price = [item.replace('$','') for item in Price if item in Price] 
 			Price = [item.replace(',','') for item in Price if item in Price]
-			Price = [(re.sub(r".*-.*",r"0",string)) for string in Price]
+			Price = [(re.sub(r"-.*",r"",string)) for string in Price]
+			Price = [(re.sub(r".*Avail.*",r"0",string)) for string in Price]
 		elif 'CHF' in string:
-		elif 'GBP'
-		elif '€'
-		elif '£'
+			indicator = 'swiss'
+			Price = [item.replace('CHF','') for item in Price if item in Price] 
+			Price = [item.replace(',','') for item in Price if item in Price]
+			Price = [(re.sub(r"-.*",r"",string)) for string in Price]
+			Price = [(re.sub(r".*Avail.*",r"0",string)) for string in Price]
+		elif 'GBP' in string:
+			indicator = 'pound'
+			Price = [item.replace('GBP','') for item in Price if item in Price] 
+			Price = [item.replace(',','') for item in Price if item in Price]
+			Price = [(re.sub(r"-.*",r"",string)) for string in Price]
+			Price = [(re.sub(r".*Avail.*",r"0",string)) for string in Price]
+		elif '€' in string:
+			indicator = 'euro'
+			Price = [item.replace('€','') for item in Price if item in Price] 
+			Price = [item.replace(',','') for item in Price if item in Price]
+			Price = [(re.sub(r"-.*",r"",string)) for string in Price]
+			Price = [(re.sub(r".*Avail.*",r"0",string)) for string in Price]
+		elif '£' in string:
+			indicator = 'pound'
+			Price = [item.replace('£','') for item in Price if item in Price] 
+			Price = [item.replace(',','') for item in Price if item in Price]
+			Price = [(re.sub(r"-.*",r"",string)) for string in Price]
+			Price = [(re.sub(r".*Avail.*",r"0",string)) for string in Price]
 
 
-
-
+	""" converts currency to USD (conversion rate taken Oct 15 via google) """
 	Price = list(map(int, Price))
-	print(Price)
+	Price = list(map(float, Price))
+	if indicator == 'euro' :
+		Price = [num * 1.16 for num in Price]
+	elif indicator == 'pound' :
+		Price = [num * 1.38 for num in Price]
+	elif indicator == 'swiss' :
+		Price = [num * 1.08 for num in Price]
+	Price = list(map(round,Price))
 
-	# for string in Price :
-	# 	Price = int(string)
-
-	""" Make date list match length with others """
+	""" Date array match length of others """
 	date = date * len(Year)
 
 	""" Put data in data frame """
-	# data = {'Lot': Lot, 'Price': Price, 'Year': Year, 'Cars': Car, 'Sold': sold,'Date': date}
-	# df = pd.DataFrame.from_dict(data,orient='index')
-	# df = df.transpose()
+	data = {'Lot': Lot, 'Price_USD': Price, 'Year': Year, 'Cars': Car, 'Sold': sold,'Date': date}
+	df = pd.DataFrame.from_dict(data,orient='index')
+	df = df.transpose()
 
-	# rnum = np.random.randint(1,1000,size=2)
+	""" Assign random variable to save .csv """
+	rnum = np.random.randint(1,1000,size=2)
 
+	df.to_csv(str(np.random.randint(1,1000,size=1)) + '.csv')
 
-	# df.to_csv(str(np.random.randint(1,1000,size=1)) + '.csv')
-
-make_soup('https://rmsothebys.com/en/home/auction-results/az16#?SortBy=Default&SearchTerm=&Category=All%20Categories&IncludeWithdrawnLots=false&Auction=AZ16&OfferStatus=All%20Availability&AuctionYear=&Model=Model&Make=Make&FeaturedOnly=false&StillForSaleOnly=false&Collection=All%20Lots&WithoutReserveOnly=false&Day=All%20Days&TimedOnly=false&OneHubLinkOnly=false&InspectionReportOnly=false&AuctionedStatus=All%20Lots&page=4&pageSize=40')
+make_soup('https://rmsothebys.com/en/home/auction-results/sm21')
 
 
 # data = {'Lot': Price_Lots, 'Sold': sold, 'Make_and_model': Model_and_Make}
