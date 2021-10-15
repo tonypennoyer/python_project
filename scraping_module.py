8286c60ec7551c82d429f7e05ac604c4d4de0005
Tony Pennoyer <tonypennoyer@gmail.com>
2021-10-15 01:14:04 -0400

    update

--- scraping_module.py
+++ scraping_module.py
@@ -4,6 +4,8 @@ import re
 import csv
 import requests
 import pandas as pd
+import numpy as np
+from itertools import chain
 from csv import writer
 from bs4 import BeautifulSoup
 
@@ -31,6 +33,7 @@ def scrape_rm(soup) :
 	sold_soup = []
 	sold = []
 	date = []
+	country_lst=['USD','EUR','GPB']
 
 	headings = soup.find_all('div', {'class': 'search-result__caption'})
 
@@ -71,24 +74,32 @@ def scrape_rm(soup) :
 		sold_soup.append(item.find('p', {'class':'clearfix'}).text)
 
 
-	sold_str = [item.split() for item in sold_soup]
+	sold_lst = [item.split() for item in sold_soup]
 
-	for lst in sold_str :
-		for index, elem in enumerate(lst) :
-			if (index+1 < len(lst) and index - 1 >= 0) :
+	sold_lst = list(chain.from_iterable(sold_lst))
 
-				prev_el = str(lst[index-1])
-				curr_el = str(elem)
-				next_el = str(lst[index+1])
+	# sold_lst = [item.replace('High','') for item in sold_lst]
+	# sold_lst = [item.replace('bidder','') for item in sold_lst]
+	# sold_lst = [item.replace('Bid:','') for item in sold_lst]
 
-				if (prev_el == 'USD' or 'EUR' or 'GPB') and curr_el == 'Not' and next_el == 'Sold':
-					sold.append(curr_el + ' ' + next_el)
-				if (prev_el == 'USD' or 'EUR' or 'GPB') and curr_el == 'Sold' and next_el == 'Current:':
-					sold.append(curr_el)
-				if curr_el == 'Bid:' and next_el == 'Sold' :
-					continue
-				else : continue
+	# print(sold_lst)
 
+	for index, elem in enumerate(sold_lst) :
+		if (index+1 < len(sold_lst) and index - 1 >= 0) :
+
+			prev_el = str(sold_lst[index-1])
+			curr_el = str(elem)
+			next_el = str(sold_lst[index+1])
+
+			# print(curr_el)
+			if prev_el in country_lst and curr_el == 'Sold' :
+				sold.append('Sold')
+			if prev_el in country_lst and curr_el == 'Not' and next_el == 'Sold':
+				sold.append('Not Sold')
+			else : continue
+
+
+	# print(len(sold))
 
 	""" Cleaning data section """
 	Model_and_Make = [item.split(' ', 1) for item in Model_and_Make]
@@ -102,28 +113,35 @@ def scrape_rm(soup) :
 	Price_Lots = [item.replace('GBP','') for item in Price_Lots]
 	Price_Lots = [item.split('|') for item in Price_Lots]
 
+	# print(sold_str)
+
 	""" Split up nested lists into separate ones """
 	Lot, Price = zip(*Price_Lots)
 	Year, Car = zip(*Model_and_Make)
 
 
-	# """ Make date list match length with others """
-	for i in range(len(Year)-1) :
-		date += date
+	""" Make date list match length with others """
+	date = date * len(Year)
 
 	""" Put data in data frame """
-	data = {'Lot': Lot, 'Price': Price, 'Year': Year, 'Car': Car, 'Sold': sold, 'Date': date}
+	data = {'Lot': Lot, 'Price': Price, 'Year': Year, 'Cars': Car, 'Sold': sold,'Date': date}
 	df = pd.DataFrame.from_dict(data,orient='index')
 	df = df.transpose()
-	print(df.head(2))
 
-make_soup('https://rmsothebys.com/en/home/auction-results/hf16')
+	# rnum = np.random.randint(1,1000,size=2)
+
+	df.to_csv(str(np.random.randint(1,1000,size=1)) + '.csv')
+
+make_soup('https://rmsothebys.com/en/home/auction-results/mo16#/&page=%7B2%7D?SortBy=Default&SearchTerm=&Category=All%20Categories&IncludeWithdrawnLots=false&Auction=MO16&OfferStatus=All%20Availability&AuctionYear=&Model=Model&Make=Make&FeaturedOnly=false&StillForSaleOnly=false&Collection=All%20Lots&WithoutReserveOnly=false&Day=All%20Days&TimedOnly=false&OneHubLinkOnly=false&InspectionReportOnly=false&AuctionedStatus=All%20Lots&page=3&pageSize=40')
 
 
 # data = {'Lot': Price_Lots, 'Sold': sold, 'Make_and_model': Model_and_Make}
 # df = pd.DataFrame(data=data)
 
+# merge = pd.concat(
+# 	map(pd.read_csv,['Mont_16_page_1.csv','Mont_16_page_2.csv','Mont_16_page_3.csv']), ignore_index=True)
 
+# merge.to_csv('Mont16.csv')
 
 
 
